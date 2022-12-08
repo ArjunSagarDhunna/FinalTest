4019ac11bc1b43ffe7beea2561e1490940aa24a5
adhunna <adhunna@myseneca.ca>
2022-12-08 15:16:06 -0500

    initial

--- final.js
+++ final.js
@@ -8,3 +8,17 @@ var Schema = new Schema({
     "unique": true} ,
   "password": String
 });
+let finalUsers;
+module.exports.startDB = function () {
+    return new Promise(function (resolve, reject) {
+        const db= mongoose.createconnection("mongodb+srv://senecaweb.rsbwdc7.mongodb.net/myFirstDatabase", {useNewUrlParser: true});
+        db.on('error', function(err){
+            console.log("Cannot connect to DB");    
+        });
+        db.once('open', function(){
+            User = mongoose.model("finalUsers", new_Schema);
+            console.log("DB connection successful.");
+            resolve();
+         });
+     });
+ };
